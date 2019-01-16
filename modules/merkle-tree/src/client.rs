c5a8bd3ff4023c6e3801286a2f65fed8e6c0ca24
filiplazovic <lazoviccorp@gmail.com>
2019-01-16 21:30:23 +0100

    Added comments

--- modules/merkle-tree/src/client.rs
+++ modules/merkle-tree/src/client.rs
@@ -22,11 +22,15 @@ pub struct Node {
 }
 
 pub struct MerkleTreeClient {
+    // Current root hash of the tree
     pub root_hash: Option<H256>,
+    // Number of leaf nodes
     pub n_nodes: u128,
     // zero based index of a node
     pub node_indices: HashMap<H256, u128>,
+    // Edge nodes neccessary for the next insert
     pub edge_nodes: EdgeNodes,
+    // Every node in tree and its connections
     pub tree: Tree,
 }
 
@@ -41,18 +45,22 @@ impl MerkleTreeClient {
         }
     }
 
+    // Builds the whole tree with events emitted by the module
     pub fn build_tree_from_events(self: &mut Self, events: Vec<H256>) {
         for event_record in events.iter() {
             self.insert(*event_record);
         }
     }
 
+    // Get proof that specified `value` is inside the tree state with root of `root_hash`
     pub fn get_proof_for(self: &Self, value: Vec<u8>, root_hash: H256) -> Result<Proof, &'static str> {
         let value_hash = BlakeTwo256::hash_of(&value);
         let proof: Vec<Option<H256>> = vec![];
+        // If `value_hash` is equal to `root_hash`, that means that tree only has one node, in which case empty proof is returned
         if value_hash == root_hash {
             return Ok(proof);
         }
+        // Load the saved tree at state `root_hash`
         let tree_result = self.load_snapshot(&root_hash);
         match tree_result {
             Ok(tree) => self.find_node(&tree, proof, value_hash, root_hash).ok_or("Proof not found!"),
@@ -60,25 +68,34 @@ impl MerkleTreeClient {
         }
     }
 
+    // Get zero based index of a specified node
     pub fn get_node_index(self: &Self, value: Vec<u8>) -> u128 {
         let value_hash = BlakeTwo256::hash_of(&value);
         *self.node_indices.get(&value_hash).unwrap()
     }
 
+    // Recursively find all the nodes needed to make the root hash `node_hash`
+    // TODO: Change the `node_hash` to `root_hash`
     fn find_node(self: &Self, tree: &Tree, mut proof: Vec<Option<H256>>, hash: H256, node_hash: H256) -> Option<Proof> {
         match tree.get(&hash) {
             Some(node) => match node.sibling {
                 Some(sibling) => {
+                    // Node has a sibling, push it to proof array
                     proof.push(Some(sibling));
+
+                    // Proof is complete
                     if node.parent == node_hash {
                         return Some(proof);
                     }
 
+                    // Going up the tree one level
                     self.find_node(&tree, proof, node.parent, node_hash)
                 },
                 None => {
+                    // Node doesnt have a sibling, push None to proof array
                     proof.push(None);
 
+                    // Going up the tree one level
                     self.find_node(&tree, proof, node.parent, node_hash)
                 },
             },
@@ -86,20 +103,26 @@ impl MerkleTreeClient {
         }
     }
 
+    // Insert a hash into the tree
     fn insert(self: &mut Self, value_hash: H256) {
         let mut pair_hash = value_hash;
         let mut new_edge = value_hash;
+        // Get the level where the next edge node is
         let next_edge_addition_level = self.count_bit_set_from_right(self.n_nodes);
 
+        // Loop throught the edge nodes and save all connections between nodes
         for i in 0..self.edge_nodes.len() {
             let edge_node = self.edge_nodes[i];
             pair_hash = match edge_node {
                 Some(hash) => {
+                    // If edge node on `i` level is not None, make a hash of pair [hash of the value, edge node]
                     let new_hash = BlakeTwo256::hash_of(&[hash, pair_hash]);
+                    // Edge node now has a sibling and a parent
                     self.tree.insert(hash, Node {
                         parent: new_hash,
                         sibling: Some(pair_hash)
                     });
+                    // New node now has a sibling (edge node) and a parent
                     self.tree.insert(pair_hash, Node {
                         parent: new_hash,
                         sibling: Some(hash)
@@ -107,7 +130,9 @@ impl MerkleTreeClient {
                     new_hash
                 },
                 None => {
+                    // No edge node on this level, just do the hash of itself
                     let new_hash = BlakeTwo256::hash_of(&pair_hash);
+                    // New node doesnt have a sibling, but it has the parent
                     self.tree.insert(pair_hash, Node {
                         parent: new_hash,
                         sibling: None
@@ -115,21 +140,33 @@ impl MerkleTreeClient {
                     new_hash
                 }
             };
+            // Save new edge node
             if (i + 1) as u8 == next_edge_addition_level {
                 new_edge = pair_hash;
             }
         }
 
+        // Update root hash
         self.root_hash = Some(pair_hash);
+        // Set node index before we increment n_nodes
         self.node_indices.insert(value_hash, self.n_nodes);
+        // Increment n_nodes
         self.n_nodes += 1;
+        // Save new edge, and remove the invalid ones
         self.update_edges(new_edge, next_edge_addition_level as usize);
 
+        // If tree has one level, no need to save snapshot
         if value_hash != pair_hash {
             self.save_snapshot(&pair_hash, &self.tree);
         }
     }
 
+    // Saves the current tree state in `snapshots` folder
+    // Snapshots will be used to check if some values exists inside `new_root_hash`
+
+    // TODO: Since we are storing tree states to cold storage,
+    // add functionality to continue syncing with on-chain tree from last saved checkpoint
+    // Information about last saved checkpoint will be saved in file checkpoint inside `snapshots` folder
     fn save_snapshot(self: &Self, new_root_hash: &H256, tree: &Tree) {
         let name = format!("src/snapshots/{:?}", new_root_hash);
         let content = serde_json::to_string_pretty(tree);
@@ -143,6 +180,7 @@ impl MerkleTreeClient {
         }
     }
 
+    // Loads the tree with state `root_hash`
     fn load_snapshot(self: &Self, root_hash: &H256) -> Result<Tree, &'static str> {
         let name = format!("src/snapshots/{:?}", root_hash);
         let file = File::open(name);
@@ -163,17 +201,25 @@ impl MerkleTreeClient {
     }
 
     fn update_edges(self: &mut Self, new_edge_value: H256, addition_at_level: usize) {
+        // If edge is on one level higher that current tree height, we push the new edge
         if addition_at_level >= self.edge_nodes.len() {
             self.edge_nodes.push(Some(new_edge_value));
+        // If not just replace the value at level
         } else {
             self.edge_nodes[addition_at_level] = Some(new_edge_value);
         }
 
+        // Remove all values below the level of new edge
         for i in 0..addition_at_level {
             self.edge_nodes[i] = None;
         }
     }
 
+    // Calculating the successive number of 1 bits, starting from the right e.g.:
+    // 0001 - 1
+    // 0010 - 0
+    // 0011 - 2
+    // 1000 - 0
     fn count_bit_set_from_right(self: &mut Self, mut num: u128) -> u8 {
         let mut len: u8 = 0;
         while (num & 1) > 0 {
--- modules/merkle-tree/src/merkle_tree.rs
+++ modules/merkle-tree/src/merkle_tree.rs
@@ -19,13 +19,17 @@ pub trait Trait: system::Trait {
 
 decl_storage! {
     trait Store for Module<T: Trait> as MerkleTree {
+        // Root hash of the tree
         RootHash get(root_hash): Option<T::Hash>;
+        // Number of nodes in the tree
         NNodes get(n_nodes): u128;
+        // Hashes of the edge nodes needed for pairing with next insert
         EdgeNodes get(edge_nodes): Vec<Option<T::Hash>>;
     }
 }
 
 decl_event!(
+    // Event fired when new addition is added. Whole tree can be derived on client from these events
     pub enum Event<T> where <T as system::Trait>::Hash {
 		Insert(Hash),
 	}
@@ -35,41 +39,53 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         fn deposit_event<T>() = default;
         pub fn insert(value: Vec<u8>) -> Result {
+            // Make a hash of value
             let value_hash = T::Hashing::hash_of(&value);
 
+            // Pair node used for hashing with edge node
             let mut pair_hash = value_hash;
             let mut new_edge = value_hash;
             let n_nodes = Self::n_nodes();
+            // Get the level on which will be the next new edge node
             let next_edge_addition_level = Self::count_bit_set_from_right(n_nodes);
 
             let edge_nodes = Self::edge_nodes();
+            // Loop trought all levels of the tree
             for i in 0..edge_nodes.len() {
                 let edge_node = edge_nodes[i];
                 pair_hash = match edge_node {
+                    // There is edge node on this level, make a hash of the pair
                     Some(hash) => T::Hashing::hash_of(&[hash, pair_hash]),
+                    // There is no edge node on this level, hash itself then
                     None => T::Hashing::hash_of(&pair_hash)
                 };
                 if (i + 1) as u8 == next_edge_addition_level {
+                    // Hash on this level is new edge
                     new_edge = pair_hash;
                 }
             }
 
+            // Update the root hash
             <RootHash<T>>::put(pair_hash);
             <NNodes<T>>::put(n_nodes + 1);
+            // Update edge nodes
             Self::update_edges(edge_nodes, new_edge, next_edge_addition_level as usize);
 
+            // Emit the event so the client can sync with the contract
             Self::deposit_event(RawEvent::Insert(value_hash));
             Ok(())
         }
 
-        pub fn verify_proof(proof: Vec<Option<T::Hash>>, value: Vec<u8>, node_n: u128, root_hash: T::Hash) -> Result {
+        // Proove that `value` hash index of `node_index` and that it exists inside `root_hash` state
+        pub fn verify_proof(proof: Vec<Option<T::Hash>>, value: Vec<u8>, node_index: u128, root_hash: T::Hash) -> Result {
             let mut value_hash = T::Hashing::hash_of(&value);
             for i in 0..proof.len() {
                 let hash = proof[i];
                 value_hash = match hash {
                     Some(h) => {
-                        let is_even = 2u128.pow(i as u32) & node_n != 0;
-                        let pair = if is_even {[h, value_hash]} else {[value_hash, h]};
+                        // Check if node on `i` level is left or right sibling
+                        let is_right = 2u128.pow(i as u32) & node_index != 0;
+                        let pair = if is_right {[h, value_hash]} else {[value_hash, h]};
                         T::Hashing::hash_of(&pair)
                     },
                     None => T::Hashing::hash_of(&value_hash),
@@ -83,12 +99,16 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
     fn update_edges(mut edge_nodes: Vec<Option<T::Hash>>, new_edge_value: T::Hash, addition_at_level: usize) {
+        // If edge is on one level higher that current tree height, we push the new edge
         if addition_at_level >= edge_nodes.len() {
             edge_nodes.push(Some(new_edge_value));
+
+        // If not just replace the value at level
         } else {
             edge_nodes[addition_at_level] = Some(new_edge_value);
         }
 
+        // Remove all values below the level of new edge
         for i in 0..addition_at_level {
             edge_nodes[i] = None;
         }
@@ -96,6 +116,11 @@ impl<T: Trait> Module<T> {
         <EdgeNodes<T>>::put(edge_nodes);
     }
 
+    // Calculating the successive number of 1 bits, starting from the right e.g.:
+    // 0001 - 1
+    // 0010 - 0
+    // 0011 - 2
+    // 1000 - 0
     fn count_bit_set_from_right(mut num: u128) -> u8 {
         let mut len: u8 = 0;
         while (num & 1) > 0 {
